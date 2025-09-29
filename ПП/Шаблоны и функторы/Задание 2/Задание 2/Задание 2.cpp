d2544de56e00a5e67d936dc163e551809f5b26e4
Aleksandra <elaginas27@gmail.com>
2025-09-29 22:33:05 +0300

    Commit 1

---
+++ ПП/Шаблоны и функторы/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+//Шаблон
+template<typename T>
+class Table {
+private:
+    vector<vector<T>> data;
+    size_t _rows;
+    size_t _cols;
+
+public:
+    // Конструктор
+    Table(size_t r, size_t c) : _rows(r), _cols(c) {
+        data.resize(_rows);
+        for (size_t i = 0; i < _rows; ++i) {
+            data[i].resize(_cols);
+        }
+    }
+
+    // Оператор [] для записи
+    vector<T>& operator[](size_t index) {
+        return data[index];
+    }
+
+    // Оператор [] для чтения
+    const vector<T>& operator[](size_t index) const {
+        return data[index];
+    }
+
+    // Функция Size
+    void Size() const {
+        cout << "Rows: " << _rows << ", Columns: " << _cols << endl;
+    }
+};
+
+int main() {
+
+    Table<int> test(2, 3);
+    test[0][0] = 4;
+    cout << test[0][0] << endl;
+    test.Size();
+
+    return 0;
+}
