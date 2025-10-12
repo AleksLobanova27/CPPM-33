fe558e4087f9297db1524c3881850c6469ff6585
Aleksandra <elaginas27@gmail.com>
2025-10-12 22:28:49 +0300

    Commit 1

---
+++ ПП/STL . 1/задание 1/задание 1/задание 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string text;
+	cout << "Введите текст: ";
+	getline(cin, text);
+
+	map<char, int> freqMap;
+	for (char c : text) {
+		freqMap[c]++;
+	}
+
+	vector<pair<char,int>> freqVector(freqMap.begin(), freqMap.end());
+	
+	sort(freqVector.begin(), freqVector.end(), [](auto& a, auto& b) {
+		return a.second > b.second;
+		});
+
+	for (const auto& p : freqVector) {
+		cout << p.first << ": " << p.second << "\n";
+	}
+	return 0;
+}
