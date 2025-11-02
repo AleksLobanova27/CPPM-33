617ac7b689f7e611cd2a7d71ecfe2ba2d1dc3e80
Aleksandra <elaginas27@gmail.com>
2025-11-02 17:33:09 +0300

    Commit 1

---
+++ ПП/STL . 2/Печать контейнера/Печать контейнера/Печать контейнера.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <string>
+using namespace std;
+
+template<typename Container>void print(const Container& container) {
+	auto it = container.begin();
+	if(it != container.end()) {
+		cout << *it;
+		++it;
+	}
+
+	while (it != container.end()) {
+		cout << ", " << *it;
+		++it;
+	}
+		
+
+}
+
+int main() {
+	set<string> test_set = { "one", "two", "three", "four" };
+	cout << "set: ";
+	print(test_set);
+	cout << endl;
+
+	list<string> test_list = { "one", "two", "three", "four" };
+	cout << "list: ";
+	print(test_list);
+	cout << endl;
+
+	vector<string> test_vector = { "one", "two", "three", "four" };
+	cout << "vector: ";
+	print(test_vector);
+
+	return 0;
+	
+}
